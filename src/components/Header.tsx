1756ab200ceb9f9277f1e2d4788906efec7391bc
amanda-santos <amandasf0200@gmail.com>
2022-06-30 20:54:00 -0300

    feat: 💄 add Header UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,30 @@
-export const Header = () => {
-  return <div></div>;
+import { Box, Flex, Icon } from "@chakra-ui/react";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
+import Image from "next/image";
+
+import logoImg from "../../public/assets/logo.svg";
+
+type HeaderProps = {
+  showBackButton?: boolean;
+};
+
+export const Header = ({ showBackButton = false }: HeaderProps) => {
+  return (
+    <Flex as="header" w="100%" h="24">
+      {showBackButton && (
+        <Icon
+          as={ChevronLeftIcon}
+          mb="auto"
+          mt="auto"
+          ml="8"
+          fontSize="32"
+          cursor="pointer"
+        />
+      )}
+
+      <Box margin="auto">
+        <Image src={logoImg} alt="World Trip logo" height={40} />
+      </Box>
+    </Flex>
+  );
 };
